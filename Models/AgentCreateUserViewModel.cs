8ffe1fe5fcbc7f57857568b4d9ebffbaa44dbaa0
Sajid Ali <greatsajidali709@gmail.com>
2025-05-09 20:06:54 +0500

    update

---
+++ Models/AgentCreateUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCourier.Models.ViewModels
+{
+    public class AgentCreateUserViewModel
+    {
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserType { get; set; } = "user";
+    }
+}
---
+++ Models/CreateUserViewModel.cs
@@ -0,0 +1,7 @@
+﻿using mycourier.Models;
+
+public class CreateUserViewModel
+{
+    public User NewUser { get; set; }
+    public List<User> Users { get; set; }
+}
